6bd6acefd7157f4c764b85351b8383a9e1c15e6f
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-05-28 00:40:10 +0530

    cache: no-store method added

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -29,7 +29,9 @@ const Feed = () => {
   
   useEffect(() => {
     (async () => {
-      const response = await fetch("/api/prompt");
+      const response = await fetch("/api/prompt", {
+        cache: "no-store"
+      });
       const data = await response.json();
       setPosts(data.reverse())
     })()
