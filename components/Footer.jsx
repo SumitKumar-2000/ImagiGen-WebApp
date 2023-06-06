67e34107c9e2f2957337eaa4b9eb4db5076b9474
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-06-06 14:06:33 +0530

    [Footer.jsx]

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='mt-auto'>
         <div className='mt-8 w-full flex-center py-1 md:py-2 text-xs md:text-sm text-gray-700 font-semibold'>
-            Copyrights &copy; 2022 - ImagiGen
+            Copyrights &copy; 2023 - ImagiGen
         </div>  
     </div>
   )
